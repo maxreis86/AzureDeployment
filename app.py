1ac440977267ffab0cfe13ccc2803a7b1291c710
maxreis86 <maxreis86@gmail.com>
2022-11-27 20:37:48 -0300

    predict with H2O

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
-import pickle
-
+import h2o
+import pandas as pd
+import json
 
 app = Flask(__name__)
-model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
+BestModelId = 'h2o_champion_titanic_propensity_survive_v1_20221127_154332.zip'
 
 @app.route('/')
 def home():
-    #return 'Hello World'
     return render_template('home.html')
-    #return render_template('index.html')
 
 @app.route('/predict',methods = ['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    print(prediction[0])
+    prediction = h2o.mojo_predict_pandas(pd.DataFrame(request.form.to_dict(), index=[0]),
+                                     mojo_zip_path=BestModelId,
+                                     genmodel_jar_path='h2o-genmodel.jar',
+                                     verbose=False).loc[:,('predict','p1')]
 
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
-
+    print(prediction['p1'][0])
+    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction['p1'][0]))
 
 if __name__ == '__main__':
     app.run(debug=True)
